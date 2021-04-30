0a699b7bcff77eedb6edb76ae2d4efdfddd879ee
Angel Zambrano <angelfguerraz@web.de>
2021-04-30 16:14:19 +0200

    changes 1

--- Kotlin/HelloWorld/src/main/kotlin/main.kt
+++ Kotlin/HelloWorld/src/main/kotlin/main.kt
@@ -1,3 +1,24 @@
+// constant
+const val PI = 3.1416
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    // var: read write variable
+    var cash = 10
+    println(cash)
+    cash = 5
+    println(cash)
+
+    // val: just read variable
+    val name = "Maria"
+    println(name)
+
+    val boo = true
+    val large_n : Long = 3L
+    val double : Double = 2.71
+    val float : Float = 1.1f
+    val n1 = 29
+    val n2 = 19
+    val n3 = n1 - n2
+    println(n3)
 }
